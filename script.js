715db3232fb7ee679ab79a06b5d6c279dd6e72bb
MatheusToscano <matheustoscano2012@gmail.com>
2023-10-16 18:25:06 -0300

    personal update

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function toggleMode() {
     const img = document.querySelector("#profile img")
 
     if (html.classList.contains('light')) {
-        img.setAttribute('src', './assets/avatar-light.png')
+        img.setAttribute('src', './assets/avatar-light2.png')
     } else {
-        img.setAttribute("src", "./assets/avatar.png")
+        img.setAttribute("src", "./assets/avatar2.png")
     }
 }
